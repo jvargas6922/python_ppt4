99ceb81955486b3319585882320c3f6ff742514a
adrian6922 <vargaszambranoj@gmaail.com>
2025-12-02 20:37:49 -0300

    avances desarrollo reserva entrada

--- sistema_reserva.py
+++ sistema_reserva.py
@@ -6,7 +6,7 @@ Contexto:
     condicionales para manejar la lógica del sistema.
 Consigna: 
     1. Mostrar una lista de películas disponibles con horarios. (listo)
-    2. Permitir al usuario seleccionar una película y la cantidad de boletos.
+    2. Permitir al usuario seleccionar una película y la cantidad de boletos.(listo)
     3. Validar la disponibilidad de boletos.
     4. Mostrar el resumen final con la película, el número de boletos y el precio total.
     5. Permitir nuevas reservas hasta que el usuario decida salir.
@@ -21,30 +21,115 @@ Paso a paso:
 """
 # Lista de peliculas con su horario y disponibilidad
 lista_horarios = ["18:00", "20:00", "16:30"]
-peliculas = {
+lista_opciones = ["1 - Avatar 2", "2 - Avengers Endgame", "3 - Toy Story 4", "4 - Salir"]
+
+lista_peliculas = {
     "Avatar 2": {
-        "horario": lista_horarios, # 18:00, 20:00, 16:30, etc
-        "boletos_disponibles": 30
+        "horario_1": "16:30",
+        "boletos_disponibles_horario_1": 30,
+        "horario_2": "18:00",
+        "boletos_disponibles_horario_2": 30,
+        "horario_3": "20:00",
+        "boletos_disponibles_horario_3": 30,
+        "horarios":["16:30", "18:00", "20:00"]
     },
     "Avengers Endgame": {
-        "horario": lista_horarios, # 18:00, 20:00, 16:30, etc
-        "boletos_disponibles": 25
+        "horario_1": "16:30",
+        "boletos_disponibles_horario_1": 25,
+        "horario_2": "18:00",
+        "boletos_disponibles_horario_2": 25,
+        "horario_3": "20:00",
+        "boletos_disponibles_horario_3": 25,
+        "horarios":["16:30", "18:00", "20:00"]
     },
     "Toy Story 4": {
-        "horario": lista_horarios, # 18:00, 20:00, 16:30, etc
-        "boletos_disponibles": 40
+        "horario_1": "16:30",
+        "boletos_disponibles_horario_1": 40,
+        "horario_2": "18:00",
+        "boletos_disponibles_horario_2": 40,
+        "horario_3": "20:00",
+        "boletos_disponibles_horario_3": 40,
+        "horarios":["16:30", "18:00", "20:00"]
     }
 }
+seleccion_usuario = {}
 
-print(peliculas)
-
-pelicula = input("Ingrese la pelicula que desea ver: ")
-# for pelicula in peliculas.keys():
-if pelicula in peliculas:
-    print("Existe la pelicula")
-    # horarios disponible de la pelicula seleccionada
-    print("Horarios disponibles: ")
-    for horario in peliculas[pelicula]["horario"]:
-        print(horario)
+print("Peliculas disponibles: ")
+for opcion in lista_opciones:
+    print(opcion)
+pelicula = input("Elija la opcion de la pelicula que desea ver: ")
+# while pelicula != "5":
+    # pelicula = input("Ingrese la pelicula que desea ver: ")
+    # for pelicula in peliculas.keys():
+    # saber que pelicula eligio el usuario
+if pelicula == "1":
+    pelicula = "Avatar 2"
+elif pelicula == "2":
+    pelicula = "Avengers Endgame"
+elif pelicula == "3":
+    pelicula = "Toy Story 4"
+elif pelicula == "4":
+    pelicula = "Salir"
 else:
-    print("No existe la pelicula")
+    pelicula = "No existe la pelicula"
+
+
+
+if pelicula in lista_peliculas:
+    print(f"Horarios disponibles para {pelicula}: {lista_peliculas[pelicula]['horarios']}")
+    print("Seleccione un horario: \n1- 16:30  \n2- 18:00 \n3- 20:00")
+    horario_seleccionado = input("Ingrese la opcion del horario que desea: ")
+    if horario_seleccionado == "1":
+        horario = lista_peliculas[pelicula]['horarios'][0]
+        boletos_disponibles = lista_peliculas[pelicula]["boletos_disponibles_horario_1"]
+    elif horario_seleccionado == "2":
+        horario = lista_peliculas[pelicula]['horarios'][1]
+        boletos_disponibles = lista_peliculas[pelicula]["boletos_disponibles_horario_2"]
+    elif horario_seleccionado == "3":
+        horario = lista_peliculas[pelicula]['horarios'][2]
+        boletos_disponibles = lista_peliculas[pelicula]["boletos_disponibles_horario_3"]
+    else:
+        horario = "No existe el horario"
+        boletos_disponibles = 0
+
+    print("Horario y Pelicula seleccionada: ", horario, pelicula)
+    cantidad_boletos =  int(input("Ingrese la cantidad de boletos que desea reservar: "))
+     # avatar => 30 , Avengers Endgame => 25, Toy Story 4 => 40
+
+    if pelicula == "Avatar 2":
+        if cantidad_boletos > 30:
+            print("No hay suficientes boletos disponibles")
+        else:
+            print("Reserva exitosa")
+    elif pelicula == "Avengers Endgame":
+        if cantidad_boletos > 25:
+            print("No hay suficientes boletos disponibles")
+        else:
+            print("Reserva exitosa")
+            # podria sacar calculos de los bolestos comprados
+    elif pelicula == "Toy Story 4":
+        if cantidad_boletos > 40:
+            print("No hay suficientes boletos disponibles")
+        else:
+            print("Reserva exitosa")
+
+
+
+"""
+{
+    pelicula = Avatar 2
+    horario = 18:00
+    boletos = 3
+},
+{
+    pelicula = Avengers Endgame
+    horario = 20:00
+    boletos = 2
+},
+{
+    pelicula = Toy Story 4
+    horario = 16:30
+    boletos = 5
+}
+
+"""
